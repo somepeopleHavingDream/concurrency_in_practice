267099b59bdc35e5e3d69b35343d7a5b39fca13b
yangxin <1393003255@qq.com>
2019-09-17 14:50:08 +0800

    feat: 停止线程的最佳实践-普通情况

---
+++ src/main/java/threadcoreknowledge/stopthreads/RightWayStopThreadWithoutSleep.java
@@ -0,0 +1,30 @@
+package threadcoreknowledge.stopthreads;
+
+/**
+ * run方法内没有sleep或wait方法时，停止线程
+ *
+ * @author yangxin
+ * 2019/09/17 14:43
+ */
+public class RightWayStopThreadWithoutSleep implements Runnable {
+
+    @Override
+    public void run() {
+        int num = 0;
+        while (!Thread.currentThread().isInterrupted() && num <= Integer.MAX_VALUE / 2) {
+//        while (num <= Integer.MAX_VALUE / 2) {
+            if (num % 10000 == 0) {
+                System.out.println(num + "是10000的倍数");
+            }
+            num++;
+        }
+        System.out.println("任务运行结束了");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new RightWayStopThreadWithoutSleep());
+        thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
+    }
+}
