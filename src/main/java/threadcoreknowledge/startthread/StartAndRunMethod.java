ae5b2f497bab1430ffe483c243ec38c6d010c853
yangxin <1393003255@qq.com>
2019-09-17 14:05:08 +0800

    feat: 完成启动线程的正确姿势

---
+++ src/main/java/threadcoreknowledge/startthread/StartAndRunMethod.java
@@ -0,0 +1,16 @@
+package threadcoreknowledge.startthread;
+
+/**
+ * 对比start和run两种启动线程的方式
+ *
+ * @author yangxin
+ * 2019/09/17 13:58
+ */
+public class StartAndRunMethod {
+    public static void main(String[] args) {
+        Runnable runnable = () -> System.out.println(Thread.currentThread().getName());
+        runnable.run();
+
+        new Thread(runnable).start();
+    }
+}
