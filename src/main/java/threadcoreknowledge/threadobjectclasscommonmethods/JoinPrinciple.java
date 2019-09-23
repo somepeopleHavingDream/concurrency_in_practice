3facd8ec09639b8c3e58e9898cd60d5fc4c5f5c8
yangxin <1393003255@qq.com>
2019-09-23 18:02:53 +0800

    feat: join注意点、原理、源码分析、替代写法、面试问题

---
+++ src/main/java/threadcoreknowledge/threadobjectclasscommonmethods/JoinPrinciple.java
@@ -0,0 +1,30 @@
+package threadcoreknowledge.threadobjectclasscommonmethods;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 通过讲解join原理，分析出join的代替写法
+ *
+ * @author yangxin
+ * 2019/09/23 17:56
+ */
+public class JoinPrinciple {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            try {
+                TimeUnit.MICROSECONDS.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "执行完毕");
+        });
+
+        thread1.start();
+        System.out.println("开始等待子线程运行完毕");
+        synchronized (thread1) {
+            thread1.wait();
+        }
+//        thread1.join();
+        System.out.println("所有子线程执行完毕");
+    }
+}
