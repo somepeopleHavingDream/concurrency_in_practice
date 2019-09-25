3bd04d4a52d10011e44aa0fb6d5b48ae99ccf9af
yangxin <1393003255@qq.com>
2019-09-25 15:54:10 +0800

    feat: 自己实现并处理异常；常见面试问题

---
+++ src/main/java/threadcoreknowledge/uncaughtexception/MyUncaughtExceptionHandler.java
@@ -0,0 +1,26 @@
+package threadcoreknowledge.uncaughtexception;
+
+import lombok.AllArgsConstructor;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 我的未处理异常处理器
+ *
+ * @author yangxin
+ * 2019/09/25 15:21
+ */
+@AllArgsConstructor
+public class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+    private String name;
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        Logger logger = Logger.getAnonymousLogger();
+        logger.log(Level.WARNING, "线程异常，终止啦" + t.getName());
+//        logger.log(Level.WARNING, "线程异常，终止啦" + t.getName(), e);
+        System.out.println(name + "捕获了异常" + t.getName() + "异常");
+//        System.out.println(name + "捕获了异常" + t.getName() + "异常" + e);
+    }
+}
---
+++ src/main/java/threadcoreknowledge/uncaughtexception/UseOwnUncaughtExceptionHandler.java
@@ -0,0 +1,26 @@
+package threadcoreknowledge.uncaughtexception;
+
+/**
+ * 使用刚才自己写的UncaughtExceptionHandler
+ *
+ * @author yangxin
+ * 2019/09/25 15:33
+ */
+public class UseOwnUncaughtExceptionHandler implements Runnable {
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler("捕获器1"));
+
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-1").start();
+        Thread.sleep(300);
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-2").start();
+        Thread.sleep(300);
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-3").start();
+        Thread.sleep(300);
+        new Thread(new UseOwnUncaughtExceptionHandler(), "MyThread-4").start();
+    }
+}
