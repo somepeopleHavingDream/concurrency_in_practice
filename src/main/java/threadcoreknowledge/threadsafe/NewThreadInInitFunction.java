acebb2b6f24b2133d264a6572158b21caeabc4a8
yangxin <1393003255@qq.com>
2019-10-03 17:17:23 +0800

    feat: 时不我待--构造函数中新建线程

---
+++ src/main/java/threadcoreknowledge/threadsafe/NewThreadInInitFunction.java
@@ -0,0 +1,35 @@
+package threadcoreknowledge.threadsafe;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 构造函数中新建线程
+ *
+ * @author yangxin
+ * 2019/10/03 17:12
+ */
+public class NewThreadInInitFunction {
+    private Map<String, String> states;
+
+    public NewThreadInInitFunction() {
+        new Thread(() -> {
+            states = new HashMap<>();
+            states.put("1", "周一");
+            states.put("2", "周二");
+            states.put("3", "周三");
+            states.put("4", "周四");
+        }).start();
+    }
+
+    public Map<String, String> getStates() {
+        return states;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        NewThreadInInitFunction newThreadInInitFunction = new NewThreadInInitFunction();
+        TimeUnit.MILLISECONDS.sleep(1000);
+        System.out.println(newThreadInInitFunction.getStates().get("1"));
+    }
+}
