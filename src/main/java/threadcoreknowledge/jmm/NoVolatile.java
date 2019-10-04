796d7e9671ded56e291725e6ee3450d5bed2c0b3
yangxin <1393003255@qq.com>
2019-10-04 17:24:07 +0800

    feat: 没有银弹--volatile不适用于a++

---
+++ src/main/java/threadcoreknowledge/jmm/NoVolatile.java
@@ -0,0 +1,34 @@
+package threadcoreknowledge.jmm;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 不适用于volatile的场景
+ *
+ * @author yangxin
+ * 2019/10/04 17:18
+ */
+public class NoVolatile implements Runnable {
+    volatile int a;
+    AtomicInteger realA = new AtomicInteger();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            a++;
+            realA.incrementAndGet();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r = new NoVolatile();
+        Thread thread1 = new Thread(r);
+        Thread thread2 = new Thread(r);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(((NoVolatile) r).a);
+        System.out.println(((NoVolatile) r).realA.get());
+    }
+}
