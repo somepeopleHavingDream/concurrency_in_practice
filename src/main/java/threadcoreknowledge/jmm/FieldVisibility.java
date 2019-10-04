715fc3f072f97a8c9fd8e532bfe8a9d54aac4c81
yangxin <1393003255@qq.com>
2019-10-04 15:57:24 +0800

    feat: 只见树木--演示可见性问题

---
+++ src/main/java/threadcoreknowledge/jmm/FieldVisibility.java
@@ -0,0 +1,44 @@
+package threadcoreknowledge.jmm;
+
+/**
+ * 演示可见性带来的问题
+ *
+ * @author yangxin
+ * 2019/10/04 15:38
+ */
+public class FieldVisibility {
+    volatile int a = 1;
+    volatile int b = 2;
+
+    private void change() {
+        a = 3;
+        b = a;
+    }
+
+    private void print() {
+        System.out.println("b = " + b + "; a = " + a);
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            FieldVisibility test = new FieldVisibility();
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                test.change();
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                test.print();
+            }).start();
+        }
+    }
+}
