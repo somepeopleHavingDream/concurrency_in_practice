150f3bef1c9eb0883c9cf87bd1a02d15a405133c
yangxin <1393003255@qq.com>
2019-10-05 15:03:07 +0800

    feat: 必然发生死锁的例子、看程序停止的信号

---
+++ src/main/java/threadcoreknowledge/deadlock/MustDeadLock.java
@@ -0,0 +1,56 @@
+package threadcoreknowledge.deadlock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 必定发生死锁的情况
+ *
+ * @author yangxin
+ * 2019/10/05 14:51
+ */
+public class MustDeadLock implements Runnable {
+    int flag = 1;
+
+    static Object o1 = new Object();
+    static Object o2 = new Object();
+
+    @Override
+    public void run() {
+        System.out.println("flag = " + flag);
+        if (flag == 1) {
+            synchronized (o1) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o2) {
+                    System.out.println("线程1成功拿到两把锁");
+                }
+            }
+        }
+        if (flag == 0) {
+            synchronized (o2) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o1) {
+                    System.out.println("线程2成功拿到两把锁");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MustDeadLock r1 = new MustDeadLock();
+        MustDeadLock r2 = new MustDeadLock();
+        r1.flag = 1;
+        r2.flag = 0;
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        t1.start();
+        t2.start();
+    }
+}
