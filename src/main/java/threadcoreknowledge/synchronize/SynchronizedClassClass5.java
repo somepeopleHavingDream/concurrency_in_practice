698592039263cafd28854bb8643ab38765aad41a
yangxin <1393003255@qq.com>
2020-01-19 15:47:25 +0800

    feat: 类锁的形式2-.class

---
+++ src/main/java/threadcoreknowledge/synchronize/SynchronizedClassClass5.java
@@ -0,0 +1,36 @@
+package threadcoreknowledge.synchronize;
+
+import java.util.concurrent.TimeUnit;
+
+public class SynchronizedClassClass5 implements Runnable {
+    private static SynchronizedClassClass5 instance1 = new SynchronizedClassClass5();
+    private static SynchronizedClassClass5 instance2 = new SynchronizedClassClass5();
+
+    @Override
+    public void run() {
+        try {
+            method();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void method() throws InterruptedException {
+        synchronized (SynchronizedClassClass5.class) {
+            System.out.println("我是类锁的第二种形式：synchronized(*.class)。我叫：" + Thread.currentThread().getName());
+            TimeUnit.MILLISECONDS.sleep(3000);
+            System.out.println(Thread.currentThread().getName() + "运行结束！");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(instance1);
+        Thread t2 = new Thread(instance2);
+        t1.start();
+        t2.start();
+        while (t1.isAlive() || t2.isAlive()) {
+
+        }
+        System.out.println("finished");
+    }
+}
