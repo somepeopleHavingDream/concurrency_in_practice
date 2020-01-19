ee908be4cfee5c664a50f7ad5f0efd5c000b5d72
yangxin <1393003255@qq.com>
2020-01-19 15:22:06 +0800

    feat: 对象锁的形式2-普通方法锁

---
+++ src/main/java/threadcoreknowledge/synchronize/SynchronizedObjectMethod3.java
@@ -0,0 +1,39 @@
+package threadcoreknowledge.synchronize;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 对象锁示例1，代码块形式
+ *
+ * @author yangxin
+ * 2020/01/19 11:21
+ */
+public class SynchronizedObjectMethod3 implements Runnable {
+    private static SynchronizedObjectMethod3 instance = new SynchronizedObjectMethod3();
+
+    @Override
+    public void run() {
+        try {
+            method();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+        while (t1.isAlive() || t2.isAlive()) {
+
+        }
+        System.out.println("finished");
+    }
+
+    public synchronized void method() throws InterruptedException {
+        System.out.println("我的对象锁的方法修饰符形式，我叫" + Thread.currentThread().getName());
+        TimeUnit.MILLISECONDS.sleep(3000);
+        System.out.println(Thread.currentThread().getName() + "运行结束！");
+    }
+}
