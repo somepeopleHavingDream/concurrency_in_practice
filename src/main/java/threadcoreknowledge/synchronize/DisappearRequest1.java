c2131a9cd054bc60d60fdaf606942f9f11ada596
yangxin <1393003255@qq.com>
2020-01-19 11:35:36 +0800

    feat: 后果的代码演示和原因分析+Synchrnozied的两种用法介绍

---
+++ src/main/java/threadcoreknowledge/synchronize/DisappearRequest1.java
@@ -0,0 +1,30 @@
+package threadcoreknowledge.synchronize;
+
+/**
+ * 消失的请求
+ *
+ * @author yangxin
+ * 2020/01/19 10:54
+ */
+public class DisappearRequest1 implements Runnable {
+    private static Integer count = 0;
+
+    private static DisappearRequest1 instance = new DisappearRequest1();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(count);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100000; i++) {
+            count++;
+        }
+    }
+}
---
+++ src/main/java/threadcoreknowledge/synchronize/SynchronizedObjectCodeBlock2.java
@@ -0,0 +1,50 @@
+package threadcoreknowledge.synchronize;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 对象锁示例1，代码块形式
+ *
+ * @author yangxin
+ * 2020/01/19 11:21
+ */
+public class SynchronizedObjectCodeBlock2 implements Runnable {
+    private static SynchronizedObjectCodeBlock2 instance = new SynchronizedObjectCodeBlock2();
+
+    private final Object lock1 = new Object();
+    private final Object lock2 = new Object();
+
+    @Override
+    public void run() {
+        synchronized (lock1) {
+            System.out.println("我是lock1。我叫" + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "lock1部分运行结束。");
+        }
+
+        synchronized (lock2) {
+            System.out.println("我是lock2。我叫" + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "lock2运行结束。");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+        while (t1.isAlive() || t2.isAlive()) {
+
+        }
+        System.out.println("finished");
+    }
+}
