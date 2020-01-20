b1370b21a04c80edbd8bbed5055e1928cf346b92
yangxin <1393003255@qq.com>
2020-01-20 10:36:16 +0800

    feat: 反编译看monitor指令

---
+++ src/main/java/threadcoreknowledge/synchronize/Decompilation14.java
@@ -0,0 +1,17 @@
+package threadcoreknowledge.synchronize;
+
+/**
+ * 反编译字节码
+ *
+ * @author yangxin
+ * 2020/01/20 10:27
+ */
+public class Decompilation14 {
+    private final Object object = new Object();
+
+    public void insert(Thread thread) {
+        synchronized (object) {
+
+        }
+    }
+}
