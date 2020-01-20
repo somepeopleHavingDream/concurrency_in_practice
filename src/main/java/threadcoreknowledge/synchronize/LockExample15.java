385de0300cd02fb0c6bca85fb9bf6a0a8b11f58a
yangxin <1393003255@qq.com>
2020-01-20 14:18:16 +0800

    feat: Synchronized缺陷

---
+++ src/main/java/threadcoreknowledge/synchronize/LockExample15.java
@@ -0,0 +1,19 @@
+package threadcoreknowledge.synchronize;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 展示Lock的方法
+ *
+ * @author yangxin
+ * 2020/01/20 14:13
+ */
+public class LockExample15 {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        lock.unlock();
+        lock.tryLock();
+    }
+}
