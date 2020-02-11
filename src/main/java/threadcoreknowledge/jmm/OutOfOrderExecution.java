81108c475c30f175dd0f37254e1cedaf50593129
yangxin <1393003255@qq.com>
2020-02-11 16:13:48 +0800

    refactor: 眼见为实--重排序的前3种可能

--- src/main/java/threadcoreknowledge/jmm/OutOfOrderExecution.java
+++ src/main/java/threadcoreknowledge/jmm/OutOfOrderExecution.java
@@ -10,8 +10,10 @@ import java.util.concurrent.CountDownLatch;
  * 2019/10/04 15:03
  */
 public class OutOfOrderExecution {
-    private volatile static int x = 0, y = 0;
-    private volatile static int a = 0, b = 0;
+    private static int x = 0, y = 0;
+//    private volatile static int x = 0, y = 0;
+    private static int a = 0, b = 0;
+//    private volatile static int a = 0, b = 0;
 
     public static void main(String[] args) throws InterruptedException {
         int i = 0;
