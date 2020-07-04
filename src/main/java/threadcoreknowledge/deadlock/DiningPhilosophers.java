dc6ed838005f2e7247c6ead092f400876b809df7
yangxin <1393003255@qq.com>
2020-07-04 16:00:29 +0800

    comment: 哲学家就餐问题演示

--- src/main/java/threadcoreknowledge/deadlock/DiningPhilosophers.java
+++ src/main/java/threadcoreknowledge/deadlock/DiningPhilosophers.java
@@ -57,11 +57,12 @@ public class DiningPhilosophers {
             Object leftChopstick = chopsticks[i];
             Object rightChopstick = chopsticks[(i + 1) % chopsticks.length];
 
-            if (i == philosophers.length - 1) {
-                philosophers[i] = new Philosopher(rightChopstick, leftChopstick);
-            } else {
-                philosophers[i] = new Philosopher(leftChopstick, rightChopstick);
-            }
+            philosophers[i] = new Philosopher(leftChopstick, rightChopstick);
+//            if (i == philosophers.length - 1) {
+//                philosophers[i] = new Philosopher(rightChopstick, leftChopstick);
+//            } else {
+//                philosophers[i] = new Philosopher(leftChopstick, rightChopstick);
+//            }
             new Thread(philosophers[i], "哲学家" + (i + 1) + "号").start();
         }
     }
