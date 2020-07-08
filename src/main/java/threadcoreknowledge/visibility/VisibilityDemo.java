e7748ff27e69a4ed42cf13e6f69b8c7e2e0016c1
yangxin <1393003255@qq.com>
2020-07-08 21:29:18 +0800

    feat：volatile关键字原理

--- src/main/java/threadcoreknowledge/visibility/VisibilityDemo.java
+++ src/main/java/threadcoreknowledge/visibility/VisibilityDemo.java
@@ -1,7 +1,14 @@
 package threadcoreknowledge.visibility;
 
 /**
- * 可见性问题
+ * 可见性问题、排序问题（未考虑）
+ *
+ * volatile关键字的作用
+ * 1：volatile变量在读和写时会添加lock指令保证缓存同步。
+ * 2：volatile读会从内存中去同步所有的共享变量，并且保证不会有任何指令越过volatile读重排序到它前面
+ * ，即volatile读后面的操作，都是使用的内存中最新的值；
+ * volatile写保证在写入时会将工作空间中的所有变量都同步到主内存中，而且所有指令都无法越过volatile写重排序到它后面，
+ * 因此它保证了，volatile写之前的所有操作最终都会同步到主内存中。
  *
  * @author yangxin
  * 2020/07/08 17:10
