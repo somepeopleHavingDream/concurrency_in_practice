afa5e474051adc56467c1b2a43c80b87f3a090f7
yangxin <1393003255@qq.com>
2020-08-07 18:02:53 +0800

    feat: ToGetLockAfterNotify

---
+++ src/main/java/threadcoreknowledge/sixstates/ToGetLockAfterNotify.java
@@ -0,0 +1,52 @@
+package threadcoreknowledge.sixstates;
+
+/**
+ * @author yangxin
+ * 2020/08/07 17:44
+ */
+public class ToGetLockAfterNotify implements Runnable {
+
+    private final Object lock1 = new Object();
+    private final Object lock2 = new Object();
+
+    private int flag;
+
+    public static void main(String[] args) {
+        ToGetLockAfterNotify toGetLockAfterNotify1 = new ToGetLockAfterNotify();
+        ToGetLockAfterNotify toGetLockAfterNotify2 = new ToGetLockAfterNotify();
+        toGetLockAfterNotify1.flag = 0;
+        toGetLockAfterNotify2.flag = 1;
+
+        new Thread(toGetLockAfterNotify1).start();
+        new Thread(toGetLockAfterNotify2).start();
+    }
+
+    @Override
+    public void run() {
+        // flag为0的线程先拿到锁1
+        if (flag == 0) {
+            synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + "拿到lock1.");
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // flag为1的线程先拿到锁2
+        if (flag == 1) {
+            synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + "拿到lock2.");
+                System.out.println(Thread.currentThread().getName() + "尝试获取lock1...");
+
+                System.out.println(Thread.currentThread() + " " + Thread.currentThread().getState());
+                synchronized (lock1) {
+                    System.out.println(Thread.currentThread().getName() + "拿到lock1.");
+                }
+            }
+        }
+    }
+}
