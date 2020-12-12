9f9fd157c4f1e027d3ed9eb2a646412a73f56d55
yangxin <1393003255@qq.com>
2020-12-12 17:16:25 +0800

    style: 中断线程的两种姿势之恢复中断

--- src/main/java/threadcoreknowledge/stopthreads/RightWayStopThreadInProd2.java
+++ src/main/java/threadcoreknowledge/stopthreads/RightWayStopThreadInProd2.java
@@ -1,9 +1,9 @@
 package threadcoreknowledge.stopthreads;
 
 /**
- * 最佳实践2：在catch子语句中调用Thread.currentThread().interrupt()来回复设置中断状态
+ * 最佳实践2：在catch子语句中调用Thread.currentThread().interrupt()来恢复设置中断状态
  * ，以便于在后续的执行中，依然能够检查到刚才发生了中断，回到刚才RightWayStopThreadInProd补上中断，
- * 让它跳出
+ * 让它跳出。
  *
  * @author yangxin
  * 2019/09/18 14:01
